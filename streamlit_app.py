805788f22a26822bc99609043b31f07c6b5e7174
glitcher-agent <55202036+glitcher-agent@users.noreply.github.com>
2025-04-11 18:45:34 +0000

    removed toekn

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 
-
-SLACK_BOT_TOKEN = st.secrets["SLACK_BOT_TOKEN"]
-print(SLACK_BOT_TOKEN)
-
-st.title("Offboarding Bot Server"+ SLACK_BOT_TOKEN)
+st.title("Offboarding Bot Server")
 st.write(
     "Server Running🔥)."
 )
